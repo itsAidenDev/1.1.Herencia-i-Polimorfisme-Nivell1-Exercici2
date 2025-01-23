6811c8a54f876d34c7633aef99cc6af0209b70cb
Aiden Romero <aidenromerogarcia@gmail.com>
2025-01-23 13:24:47 +0100

    Update Car.java
    Class "Car" changed

--- src/vehicle/Car.java
+++ src/vehicle/Car.java
@@ -1,33 +1,12 @@
 package vehicle;
 
 public class Car {
-    private String brand;
-    private String model;
-    private int hp;
+    private static final String brand = "";
+    private static String model;
+    private final int hp;
 
     public Car(String brand, String model, int hp) {
-        this.brand = brand;
         this.model = model;
-        this.hp = hp;
-    }
-
-    public String getBrand() {
-        return this.brand;
-    }
-    public String getModel() {
-        return this.model;
-    }
-    public int getHp() {
-        return this.hp;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-    public void setModel(String model) {
-        this.model = model;
-    }
-    public void setHp(int hp) {
         this.hp = hp;
     }
 
